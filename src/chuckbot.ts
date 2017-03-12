fa1bf56f14776516655b44b986554843b6ae6db9
Christoph Kau <kau.christoph@gmail.com>
2017-03-12 22:13:41 +0100

    fixed encoding of chuck jokes

--- src/chuckbot.ts
+++ src/chuckbot.ts
@@ -1,6 +1,7 @@
 'use strict'
 
 let nconf = require('nconf');
+let Entities = require('html-entities').AllHtmlEntities;
 
 import * as Bot from './bot';
 import * as Gurubot from './gurubot';
@@ -10,15 +11,17 @@ import { Helpers } from './helpers';
 class ChuckBot implements Bot {
 
     gurubot: Gurubot;
+    entities: any;
 
     constructor(gurubot: Gurubot) {
         this.gurubot = gurubot;
+        this.entities = new Entities();
     }
 
     init(): void {
         this.gurubot.controller.hears('\\+chuck', 'ambient', (bot, message) => {
             Helpers.getJSONFromUrl(nconf.get('norrisapi_url')).then((fact) => {
-                bot.reply(message, fact.value.joke);
+                bot.reply(message, this.entities.decode(fact.value.joke));
             });
         });
     }
